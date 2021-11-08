7fa9cb9dc5e9e6e87943de67f8f02f456fc2a790
Avivit Weissman <wavivit@gmail.com>
2021-11-08 13:11:00 -0800

    refactor onVideoSelect to be inline

--- src/components/App.js
+++ src/components/App.js
@@ -22,9 +22,6 @@ const App = () => {
     setVideos(response.data.items);
     setSelectedVideo(response.data.items[0]);
   };
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
 
   return (
     <div className="ui container">
@@ -35,7 +32,10 @@ const App = () => {
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList
+              onVideoSelect={(video) => setSelectedVideo(video)}
+              videos={videos}
+            />
           </div>
         </div>
       </div>
